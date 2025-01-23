4709e0ffad3bcd9ece8206718eadc1100f74d745
Drshika Asher <67125579+drshika@users.noreply.github.com>
2025-01-23 12:25:46 -0500

    use env variables

--- src/config/keycloak.ts
+++ src/config/keycloak.ts
@@ -1,7 +1,7 @@
 const keycloakConfig = {
-    realm: 'myrealm',
-    clientId: 'uiuc-chat',
-    url: 'http://localhost:8080',
-  }
-  
-  export default keycloakConfig
+  realm: process.env.NEXT_PUBLIC_KEYCLOAK_REALM || 'myrealm',
+  clientId: process.env.NEXT_PUBLIC_KEYCLOAK_CLIENT_ID || 'UIUCchat_vercel',
+  url: process.env.NEXT_PUBLIC_KEYCLOAK_URL || 'https://keycloak-production-8379.up.railway.app',
+}
+
+export default keycloakConfig
